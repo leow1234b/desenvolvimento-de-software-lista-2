4e57686a611cc1a28683666278540273365862ea
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-25 20:42:02 -0300

    quinto commit

---
+++ Exercicio5.java
@@ -0,0 +1,39 @@
+public class Exercicio5 {
+    public static void executar(){
+        int a = Prompt.lerInteiro("Digite o valor de a: ");
+        int b = Prompt.lerInteiro("Digite o valor de b: ");
+        
+        
+        int temp = a;
+        a = b;
+        b = temp;
+
+        
+        Prompt.imprimir("Valores trocados: a = " + a + ", b = " + b);
+    }
+
+        
+
+
+}
+    
+
+
+        
+    
+
+        
+    
+
+    
+            
+            
+        
+    
+
+
+    
+
+    
+        
+
