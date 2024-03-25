d1940de2c01a06a7a97e607a7f611c6754604cf0
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-25 20:27:25 -0300

    segundo commit

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+public class Exercicio2 {
+    public static void executar(){
+        int numero1 = Prompt.lerInteiro("Digite o primeiro número:");
+        int numero2 = Prompt.lerInteiro("Digite o segundo número:");
+
+        Prompt.imprimir("A soma dos números é: " + (numero1 + numero2));
+    }
+}
+    
+
