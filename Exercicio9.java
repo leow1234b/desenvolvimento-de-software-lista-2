a4c0e7062d13f27dd2ab77fe7050d28af8cf2a19
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-26 15:48:10 -0300

    nono commit

---
+++ Exercicio9.java
@@ -0,0 +1,20 @@
+public class Exercicio9 {
+    public static void executar(){
+        int a= Prompt.lerInteiro("Digite o primeiro número a");
+        int b = Prompt.lerInteiro("Digite o segundo número b ");
+
+        if (a >= b) {
+            Prompt.imprimir("Os números são iguais.");
+        } else {
+            Prompt.imprimir("numero maior");
+            
+        }
+    }
+}
+
+
+
+    
+
+    
+
