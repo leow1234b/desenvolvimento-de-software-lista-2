cb5d58a747ed4a44afe7fe7dc6066901e7b3481c
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-26 15:44:00 -0300

    sexto commit

---
+++ Exercicio6.java
@@ -0,0 +1,16 @@
+public class Exercicio6 {
+   public static void executar() {
+    
+   
+      
+   double temperatura = Prompt.lerDecimal("Digite a temperatura em Celsius: ");
+   double fahrenheit = (9 * temperatura + 160) / 5;
+   Prompt.imprimir("A temperatura em Fahrenheit é: " + fahrenheit);
+}
+}
+
+
+
+      
+    
+
