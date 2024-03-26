d09adda889a6799c779aeb697cf2be9be7d5ac9f
leow1234b <leonardo.cadenasantos@gmail.com>
2024-03-26 15:45:46 -0300

    setimo commit

---
+++ Exercicio7.java
@@ -0,0 +1,38 @@
+public class Exercicio7 {
+
+ public static void executar(){
+    
+    
+    
+        int numero = Prompt.lerInteiro("Digite um número: ");
+            
+            if (numero >= 100 && numero <= 200) {
+                Prompt.lerInteiro("O número está no intervalo entre 100 e 200.");
+            } else {
+                Prompt.lerInteiro("O número está fora do intervalo entre 100 e 200.");
+            }
+
+
+ }
+
+
+} 
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+
+    
+
+    
+
